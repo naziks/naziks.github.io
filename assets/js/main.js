c873e0933f55279f41318cfa1e7d158286fa593d
naziks <47501073+naziks@users.noreply.github.com>
2019-07-10 22:14:44 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	particlesJS.load('particles', 'assets/particles.json',function(){
-		$("#particles").fadeIn("slow");
+		setTimeout(function(){
+			$("#particles").fadeIn("slow");
+		},100)
 	});
 });
